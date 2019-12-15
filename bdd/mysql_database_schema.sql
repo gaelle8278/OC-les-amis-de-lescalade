b57f2720963589ce6740594efb53694d462abb9f
gaelle <gaelle.rauffet@kiwiscan.net>
2019-12-15 19:27:55 +0100

    nettoyage + ajout fichiers sql

---
+++ bdd/mysql_database_schema.sql
@@ -0,0 +1,243 @@
+-- MariaDB dump 10.17  Distrib 10.4.6-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: lesamisdelescalade
+-- ------------------------------------------------------
+-- Server version	10.4.6-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `area`
+--
+
+DROP TABLE IF EXISTS `area`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `area` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `description` mediumtext DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `spot_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_Sector_Site1_idx` (`spot_id`),
+  CONSTRAINT `fk_area_spot` FOREIGN KEY (`spot_id`) REFERENCES `spot` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `booking`
+--
+
+DROP TABLE IF EXISTS `booking`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `booking` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` varchar(45) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `status_date` datetime DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `guidebook_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_Loan_User1_idx` (`user_id`),
+  KEY `fk_Loan_Topo1_idx` (`guidebook_id`),
+  CONSTRAINT `fk_loan_guidebook` FOREIGN KEY (`guidebook_id`) REFERENCES `guidebook` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fk_loan_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) DEFAULT NULL,
+  `content` mediumtext DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `spot_id` int(11) DEFAULT NULL,
+  `moderator_notice` mediumtext DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_Comment_User1_idx` (`user_id`),
+  KEY `fk_Comment_Site1_idx` (`spot_id`),
+  CONSTRAINT `fk_comment_spot` FOREIGN KEY (`spot_id`) REFERENCES `spot` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comment_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `guidebook`
+--
+
+DROP TABLE IF EXISTS `guidebook`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `guidebook` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `description` mediumtext DEFAULT NULL,
+  `release_date` date DEFAULT NULL,
+  `place` varchar(45) DEFAULT NULL,
+  `bookable` tinyint(1) DEFAULT NULL,
+  `available` tinyint(1) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_Topo_User1_idx` (`user_id`),
+  CONSTRAINT `fk_guidebook_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `pitch`
+--
+
+DROP TABLE IF EXISTS `pitch`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pitch` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `number` varchar(45) DEFAULT NULL,
+  `notice` longtext DEFAULT NULL,
+  `grade` varchar(5) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `route_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_Length_Path1_idx` (`route_id`),
+  CONSTRAINT `fk_pitch_route` FOREIGN KEY (`route_id`) REFERENCES `route` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role` varchar(25) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `route`
+--
+
+DROP TABLE IF EXISTS `route`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `route` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `grade` varchar(5) DEFAULT NULL,
+  `number` varchar(20) DEFAULT NULL,
+  `notice` longtext DEFAULT NULL,
+  `nb_spits` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `area_id` int(11),
+  PRIMARY KEY (`id`),
+  KEY `fk_route_area_idx` (`area_id`),
+  CONSTRAINT `fk_route_area` FOREIGN KEY (`area_id`) REFERENCES `area` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `spot`
+--
+
+DROP TABLE IF EXISTS `spot`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `spot` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `description` longtext DEFAULT NULL,
+  `notice` text DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `type` varchar(20) DEFAULT NULL,
+  `region` varchar(45) DEFAULT NULL,
+  `orientation` varchar(20) DEFAULT NULL,
+  `min_height` int(11) DEFAULT NULL,
+  `max_height` int(11) DEFAULT NULL,
+  `nb_routes` int(11) DEFAULT NULL,
+  `min_grade` varchar(40) DEFAULT NULL,
+  `max_grade` varchar(40) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `tag` tinyint(4) NOT NULL DEFAULT 0,
+  `nb_areas` int(11) DEFAULT NULL,
+  `nb_pitches` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_Site_User1_idx` (`user_id`),
+  CONSTRAINT `fk_spot_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(60) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT 0,
+  `pseudo` varchar(45) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `checkedCGU` tinyint(1) NOT NULL DEFAULT 0,
+  `phone` varchar(10) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `postal_code` varchar(5) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQUE_email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=126 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_role`
+--
+
+DROP TABLE IF EXISTS `user_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_role` (
+  `role_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`role_id`,`user_id`),
+  KEY `fk_userrole_user` (`user_id`),
+  CONSTRAINT `fk_userole_role` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fk_userrole_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-12-15 19:18:38
