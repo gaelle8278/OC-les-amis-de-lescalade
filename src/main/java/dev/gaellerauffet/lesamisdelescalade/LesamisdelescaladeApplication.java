33c1b7220a7be32de4f4bd99c5ee2aa9afb1d213
gaelle <gaelle.rauffet@kiwiscan.net>
2019-12-16 19:12:20 +0100

    nettoyage du code mort de la classe Main

--- src/main/java/dev/gaellerauffet/lesamisdelescalade/LesamisdelescaladeApplication.java
+++ src/main/java/dev/gaellerauffet/lesamisdelescalade/LesamisdelescaladeApplication.java
@@ -11,7 +11,6 @@ import dev.gaellerauffet.lesamisdelescalade.model.User;
 import dev.gaellerauffet.lesamisdelescalade.persistance.UserRepository;
 
 
-
 @SpringBootApplication
 public class LesamisdelescaladeApplication {
 	
@@ -20,40 +19,6 @@ public class LesamisdelescaladeApplication {
 	public static void main(String[] args) {
 	    SpringApplication.run(LesamisdelescaladeApplication.class, args);
 	}
-
-	/*@Bean
-	public CommandLineRunner demo(UserRepository repository) {
-	    return (args) -> {
-	      // save a few customers
-	      repository.save(new User("Luc", "Clinm", "luc@hotmail.com"));
-
-	      // fetch all customers
-	      log.info("Customers found with findAll():");
-	      log.info("-------------------------------");
-	      for (User user : repository.findAll()) {
-	        log.info(user.toString());
-	      }
-	      log.info("");
-
-	      // fetch an individual customer by ID
-	      User user = repository.findById(1);
-	      log.info("Customer found with findById(1):");
-	      log.info("--------------------------------");
-	      log.info(user.toString());
-	      log.info("");
-
-	      // fetch customers by last name
-	      log.info("Customer found with findByLastName('Bauer'):");
-	      log.info("--------------------------------------------");
-	      repository.findByLastName("Bbuer").forEach(bauer -> {
-	        log.info(bauer.toString());
-	      });
-	      // for (Customer bauer : repository.findByLastName("Bauer")) {
-	      //  log.info(bauer.toString());
-	      // }
-	      log.info("");
-	    };
-	  }*/
 
 
 }
